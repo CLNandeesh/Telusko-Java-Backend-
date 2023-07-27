f5d0abdd0c148fe5322a01332350596719e8bd7e
nandeesh <nandeesh.apjkalam@gmail.com>
2023-07-27 17:31:45 +0530

    String Class code added 23 July 2023

---
+++ src/stringdemo/StringBufferDemo.java
@@ -0,0 +1,15 @@
+package stringdemo;
+
+public class StringBufferDemo {
+
+	public static void main(String[] args) {
+		
+		StringBuffer sb=new StringBuffer("Appu");
+		sb.append("raj");
+		System.out.println(sb);
+		
+		//StringBuffer sb2="Shivu";
+
+	}
+
+}
---
+++ src/stringdemo/StringDemo1.java
@@ -0,0 +1,17 @@
+package stringdemo;
+
+public class StringDemo1 {
+
+	public static void main(String[] args) {
+		
+		String s1=new String("Appu");
+		String s2="Appu";
+		String s3="Appu";
+		
+		System.out.println(s1.equals(s2));
+		System.out.println(s1==s2);
+		System.out.println(s2==s3);
+
+	}
+
+}
